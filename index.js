1a1c8ad0eb29c0116a690625a81f62e19d2c9c8b
+ <+githinjiadreen27@gmail.com>
2025-06-17 22:23:06 +0300

    DOM lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+//Remove the <main> tag element
+const main = document.getElementById('main');
+main.remove();
+
+//Create a new <h1> element and assign it to a variable
+const newHeader = document.createElement('h1');
+
+// 3. Set the id of the new <h1> to 'victory'
+newHeader.id = 'victory';
+
+// 4. Set the text content of the <h1>
+newHeader.textContent = "ADREEN is the champion"; // Replace ADREEN with your name
+
+// 5. Append the new <h1> to the body
+document.body.append(newHeader);
